3f974ef8a159563d10fc7850cd1f9a604a70fd28
huasheng <liushenghua88@126.com>
2022-04-24 17:51:05 +0800

    xiudingbanben

--- 11.py
+++ 11.py
@@ -1,3 +1,6 @@
+# 课程合计5.5个月。每周课上学习和课下练习时间约15h。2天完成一周，每天8h
+#
+# 开发时间：2022/4/24  17:43
 # This is a sample Python script.
 11111111111111
 # Press Shift+F10 to execute it or replace it with your code.
@@ -7,10 +10,3 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
